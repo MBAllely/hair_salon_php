de3ccd7b1700e6c5d07437aaf5af74c43aea76b1
Marika <mballely@gmail.com>
2016-02-26 08:54:56 -0800

    add getName funciton and specs

--- src/Stylist.php
+++ src/Stylist.php
@@ -2,7 +2,42 @@
 
 class Stylist
 {
-  
+    private $name;
+    private $phone;
+    private $id;
+
+    function __construct($name, $phone, $id)
+    {
+        $this->name = $name;
+        $this->phone = $phone;
+        $this->id = $id;
+    }
+
+    function setName($new_name)
+    {
+        $this->name = (string) $new_name;
+    }
+
+    function setPhone($new_phone)
+    {
+        $this->phone = (int) $new_phone;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getPhone()
+    {
+        return $this->phone;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
 }
 
 
--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -10,17 +10,23 @@
     $server = 'mysql:host=localhost;dbname=hair_salon_test';
     $username = 'root';
     $password = 'root';
-    $DB = new PDO($server, $username, $passoword);
+    $DB = new PDO($server, $username, $password);
 
     class ClassStylist extends PHPUnit_Framework_TestCase
     {
-        function test_methodToTest_inputDescription()
+        function test_getName()
         {
             // Arrange
+            $name = "Linda";
+            $phone = 9075553838;
+            $id = null;
+            $test_stylist = new Stylist($name, $phone, $id);
 
             // Act
+            $result = $test_stylist->getName();
 
             // Assert
+            $this->assertEquals($name, $result);
         }
     }
  ?>
