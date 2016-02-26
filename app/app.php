77c1173a4563394de0bcf3da7620a09f4e838da4
Marika <mballely@gmail.com>
2016-02-26 13:26:05 -0800

    add delete stylists functionality

--- app/app.php
+++ app/app.php
@@ -23,13 +23,31 @@
         ));
     });
 
-    
+    $app->get("/stylists{id}", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        // $clients = Client::find($id);
+
+        return $app['twig']->render('stylists.html.twig', array(
+            'stylist' => $stylist,
+            // 'clients' = $clients
+        ));
+        //YOU STOPPED HERE! RENDER YOUR PAGE, FUCK WITH IT UNTIL IT WORKS, AND THEN START ADDING DELETE/UPDATE BUTTONS.
+
+        //NEXT: ADD THE CLIENT CLASS
+    });
 
     $app->post("/stylists", function() use ($app) {
         $stylist = new Stylist($_POST['name']);
         $stylist->save();
         return $app['twig']->render('index.html.twig',
         array(
+            'stylists' => Stylist::getAll()
+        ));
+    });
+
+    $app->post("/delete_stylists", function() use ($app) {
+        Stylist::deleteAll();
+        return $app['twig']->render('index.html.twig', array(
             'stylists' => Stylist::getAll()
         ));
     });
